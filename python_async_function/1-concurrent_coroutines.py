adb14f353e7795421ed67ef4e81feeb06e5a5ac9
Chidera0001 <c.anele@alustudent.com>
2025-01-07 16:45:49 +0000

    properly imported the previous file

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
+
 """
-Asynchronous coroutine that spawns wait_randomn
-times with the specified max_delay
+Asynchronously spawn the wait_random caroutine `n`
+times with the specified `max_delay`
 """
+
+
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+
+# import the wait_random caroutine from the file 0-basic_async_syntax.py
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """This function creates a list of wait_random caroutines and uses
+    asyncio.as_completed to ensure the results are obtained in ascending order.
+    The resulting delays are collected in the 'delays' list and returned
     """
-    Spawn wait_random n times with specified max_delay
-    Args:
-        n: number of times to spawn wait_random
-        max_delay: maximum delay between spawns
-    Returns:
-        List of delays in ascending order without using sort()
-    """
-    tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
 
     for task in asyncio.as_completed(tasks):
         delay = await task
